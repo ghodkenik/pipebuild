251781a41a54ba1ee45765684d6713f1cfca0408
ghodkenik <ghodkenikhil@gmail.com>
2023-02-12 14:32:45 +0530

    building new version

--- src/main/java/com/test/springtest/HelloController.java
+++ src/main/java/com/test/springtest/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! ... yay 123 !";
+		return "Greetings from Spring Boot! ... yay 1010 from jenkins build !";
 	}
 
 }
